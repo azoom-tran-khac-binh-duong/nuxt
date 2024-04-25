27fa92eda8e58db591f6110c1fd3d7075b8bf914
duo <tran.khac.binh.duong@azoom.jp>
2024-04-25 11:23:13 +0700

    :+1: Add styles and fonts configuration

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="home-page">
+    <h1 class="title">
+      Hello World!
+    </h1>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.home-page {
+  > .title {
+    color: $color-primary;
+  }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', ['@nuxtjs/google-fonts', {
+    families: {
+      'DM Sans': [300, 400, 700],
+    },
+    display: 'swap',
+  }], '@nuxtjs/fontaine'],
   eslint: {
     config: {
       stylistic: true,
+    },
+  },
+  css: ['@/assets/styles/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/styles/abstracts/_index.scss";',
+        },
+      },
     },
   },
 })
