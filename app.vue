5934738f6ab56a4ea060b6da341a2601dc4c15df
duo <tran.khac.binh.duong@azoom.jp>
2024-04-29 16:44:59 +0700

    :+1: Add nuxt pages config

--- app.vue
+++ app.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="home-page">
-    <h1 class="title">
-      Hello World!
-    </h1>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-
-<style lang="scss" scoped>
-.home-page {
-  > .title {
-    color: $color-primary;
-  }
-}
-</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home-page">
+    <pre>
+        {{ todoList }}
+    </pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Todo {
+  userId: number
+  id: number
+  title: string
+  completed: boolean
+}
+
+const todoList = await $fetch<Todo[]>(
+  'https://jsonplaceholder.typicode.com/todos',
+)
+</script>
